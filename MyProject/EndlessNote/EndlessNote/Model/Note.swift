2469e3979991e2bd347cf61d9aa032ab77da9d2c
mertozan <mertozan1997@gmail.com>
2024-03-21 03:34:54 +0300

    MVVM created

---
+++ MyProject/EndlessNote/EndlessNote/Model/Note.swift
@@ -0,0 +1,16 @@
+//
+//  Note.swift
+//  EndlessNote
+//
+//  Created by Mert ÖZAN on 21.03.2024.
+//
+
+import Foundation
+
+struct Note {
+    var title : String
+    var content : String
+    var timetamp: Date
+}
+
+
